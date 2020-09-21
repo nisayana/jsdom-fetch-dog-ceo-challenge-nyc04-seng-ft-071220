48b948e5cbcfcb96fe2ae949a16c0a0d8f70d294
nisayana <nimaevasb@gmail.com>
2020-09-21 12:42:16 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+let breeds = []
+
+document.addEventListener("DOMContentLoaded", (evt) => {
+    // console.log(evt)
+    // evt.preventDefault()
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(res => res.json())
+        .then((dogObj) => {
+            // console.log(dogObj)
+            dogObj.message.forEach(image => addImgToDOM(image))
+        });
+
+        let addImgToDOM = (dogPicURL) => {
+            let container = document.querySelector("#dog-image-container");
+            let newImage = document.createElement("img");
+            newImage.src = dogPicURL;
+            container.appendChild(newImage)
+            // console.log(container)
+        }
+
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then((dogObj) => {
+        breeds = Object.keys(dogObj.message)
+        // console.log(breeds)
+    })
+
+
+
+})
